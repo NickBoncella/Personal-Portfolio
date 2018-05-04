6276587652f246c23ba7906e02fc3e25e1a19780
Nick Boncella <njb371@protonmail.com>
2018-05-04 04:55:07 -0400

    Add footer

---
+++ src/script.js
@@ -0,0 +1,5 @@
+/*** CURRENT YEAR FOR FOOTER ***/
+const copyright = document.querySelector('.copyright');
+const year = new Date().getFullYear();
+
+copyright.innerHTML = `Nick Boncella &copy; ${year}`;
