aa2100253f27745a9a8ef53c0491fd06df4971e5
Nick Boncella <njb371@protonmail.com>
2018-05-05 13:21:15 -0400

    Add intro animation on scroll

--- src/script.js
+++ src/script.js
@@ -1,7 +1,43 @@
-/*** CURRENT YEAR FOR FOOTER ***/
+/**** CURRENT YEAR FOR FOOTER ****/
 const copyright = document.querySelector('.copyright');
 const year = new Date().getFullYear();
 
 copyright.innerHTML = `Nick Boncella &copy; ${year}`;
 
-/*** SLIDE IN INTRO ON SCROLL ***/
+/**** SLIDE IN INTRO ON SCROLL ****/
+/* debounce - prevent function from constantly firing */
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+/* store section to be slid up in var */
+const slideIntro = document.getElementById('slide-up');
+/* add event listner to window */
+window.addEventListener('scroll', debounce(slideUp));
+
+/* SLIDEUP function */
+function slideUp(e) {
+  /* scroll thru intro section */
+  const slideUpAt = (window.scrollY + window.innerHeight) - (slideIntro.scrollHeight / 3);
+  /* bottom of intro section */
+  const imageBottom = slideIntro.offsetTop + slideIntro.scrollHeight;
+  const isHalfShown = slideUpAt > slideIntro.offsetTop;
+  const isNotScrolledPast = window.scrollY < imageBottom;
+  if (isHalfShown && isNotScrolledPast) {
+    slideIntro.classList.add('active');
+  } else {
+    slideIntro.classList.remove('active');
+  }
+}
