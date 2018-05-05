78c0c89186c3cc54ee59284741bda9c5f8c5f9e2
Nick Boncella <njb371@protonmail.com>
2018-05-05 11:48:42 -0400

    Edit footer and portfolio section

--- src/script.js
+++ src/script.js
@@ -3,3 +3,5 @@ const copyright = document.querySelector('.copyright');
 const year = new Date().getFullYear();
 
 copyright.innerHTML = `Nick Boncella &copy; ${year}`;
+
+/*** SLIDE IN INTRO ON SCROLL ***/
